5aa2b49ba9f8cc1675169414647d65bf5f655992
louisoporto042 <louisoporto042@csu.fullerton.edu>
2025-04-25 13:01:50 -0700

    Delete uneeded files

--- ftp_client.hpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-
-bool clientInit(const char* host, const char* port);
-
-void clientMainLoop();
