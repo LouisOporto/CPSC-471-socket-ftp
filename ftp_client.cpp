b54ad3e6c3ad78e6981d3b4598102a80745ea455
louisoporto042 <louisoporto042@csu.fullerton.edu>
2025-04-25 17:18:57 -0700

    Individual cpp files for server and client

--- ftp_client.cpp
+++ ftp_client.cpp
@@ -1,4 +1,8 @@
-#include "ftp_client.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 
 bool clientInit(const char* host, const char* port) {
@@ -10,4 +14,25 @@ bool clientInit(const char* host, const char* port) {
 
 void clientMainLoop() {
 
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Invalid usage: ./ftp_client <host> <port>\n");
+        return -1;
+    }
+
+    printf("Running FTP client\n");
+    
+    const char* host = "localhost";
+    const char* port = "12345";
+    if (clientInit(host, port) == 0) {
+        fprintf(stderr, "Failed to run client\n");
+        return -1;
+    }
+
+    clientMainLoop();
+
+    printf("Closing FTP client...\n");
+    return 0;
 }
--- ftp_server.cpp
+++ ftp_server.cpp
@@ -1,4 +1,9 @@
-#include "ftp_server.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 
 bool serverInit(const char* port) {
@@ -9,4 +14,24 @@ bool serverInit(const char* port) {
 
 void serverMainLoop() {
 
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Invalid usage: ./ftp_server <port>\n");
+        return -1;
+    }
+
+    printf("Running FTP server\n");
+    
+    const char* port = "12345";
+    if (serverInit(port) == 0) {
+        fprintf(stderr, "Failed to run server\n");
+        return -1;
+    }
+
+    serverMainLoop();
+
+    printf("Closing FTP server...\n");
+    return 0;
 }
