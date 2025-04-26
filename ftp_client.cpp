ab5e656eced03c553f9501d7e307057516337308
louisoporto042 <louisoporto042@csu.fullerton.edu>
2025-04-25 17:44:40 -0700

    Remove functions just main function

--- ftp_client.cpp
+++ ftp_client.cpp
@@ -5,17 +5,6 @@
 #include <sys/socket.h>
 
 
-bool clientInit(const char* host, const char* port) {
-    bool success = true;
-    
-
-    return success;
-}
-
-void clientMainLoop() {
-
-}
-
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Invalid usage: ./ftp_client <host> <port>\n");
@@ -26,12 +15,10 @@ int main(int argc, char* argv[]) {
     
     const char* host = "localhost";
     const char* port = "12345";
-    if (clientInit(host, port) == 0) {
-        fprintf(stderr, "Failed to run client\n");
-        return -1;
-    }
+    sockaddr_in srvaddr;
 
-    clientMainLoop();
+    int sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
 
     printf("Closing FTP client...\n");
     return 0;
--- ftp_server.cpp
+++ ftp_server.cpp
@@ -6,17 +6,8 @@
 #include <sys/socket.h>
 
 
-bool serverInit(const char* port) {
-    bool success = true;
-
-    return success;
-}
-
-void serverMainLoop() {
-
-}
-
 int main(int argc, char* argv[]) {
+    // const char* port = 0;
     if (argc != 2) {
         fprintf(stderr, "Invalid usage: ./ftp_server <port>\n");
         return -1;
@@ -25,12 +16,10 @@ int main(int argc, char* argv[]) {
     printf("Running FTP server\n");
     
     const char* port = "12345";
-    if (serverInit(port) == 0) {
-        fprintf(stderr, "Failed to run server\n");
-        return -1;
-    }
+    sockaddr_in srvaddr;
+    
 
-    serverMainLoop();
+    int sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     printf("Closing FTP server...\n");
     return 0;
